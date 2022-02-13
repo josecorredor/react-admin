8621a7be9444d9f107c80adda095ae8efd07763e
josecorredor <jose.corredor@linqueate.com>
2022-02-13 14:04:13 +1000

    IndexStyles

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,46 @@
 import axios from 'axios';
 import React, { SyntheticEvent, useState } from 'react';
 import { Navigate } from 'react-router-dom';
+import '../Login.css';
 
 const Login = () => {
     const[email, setEmail] = useState('');
     const[password, setPassword] = useState('');
     const[redirect, setRedirect] = useState(false);
+    const[hidden, setHidden] = useState(false);
 
     const submit = async (e: SyntheticEvent) => {
         e.preventDefault();
 
-        const {data}= await axios.post('/login', {
-            email,
-            password
-        });
-        setRedirect(true);
+        (async() => {
+            let apiRes = null;
+            try {
+                const {data}= await axios.post('/login', {
+                    email,
+                    password
+                });
+                setRedirect(true);
+            } catch (err:any) {
+                setHidden(true);
+                apiRes = err.response.data.message;
+                const element: HTMLElement = document.getElementById('messageForm') as HTMLElement
+                element.innerHTML = apiRes;
+                
+              } finally {
+                //setHidden(true);
+                console.log(apiRes); 
+              }
+            })();
     }
+
     if (redirect){
         return <Navigate to={'/'}/>;
     }
 
     return (
         <main className="form-signin">
+            <div className="row justify-content-center align-items-center vh-100">
+                <div className='col-sm-3 bg-white'>
             <form onSubmit={submit}>
                 <h1 className="titleRegister">Sign In</h1>
                 <div className="form-floating">
@@ -33,9 +52,14 @@ const Login = () => {
                 <label htmlFor="floatingPassword">Password</label>
                 </div>
 
-                <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+                <button className="m3 w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+                {hidden ? <div id='messInfor' className="messInfo">
+                    <div id='messageForm' className="alert alert-info" role="alert"></div>
+                </div>:null}
                 <p className="copy">&copy; 2021</p>
             </form>
+            </div>
+            </div>
         </main>
     )
 };
